f3373e8d19f45747221e17ed33829ab1447b49a9
adams <adamskiptalam0@gmail.com>
2025-03-13 16:37:33 +0300

    Moved the logic for getting the pokemon data from the api

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,41 @@
-import FetchPokemons from "./FetchPokemonData";
+import { useState, useEffect } from "react";
+
+import FetchPokemons from "./PokemonCard";
+import '../Styles/PokemonData.css';
+
+
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [score, setScore] = useState(0);
+  const [bestScore, SetBestScore] = useState(0);
+  const [clickedPokemon, setClickedPokemon] = useState(new Set());
+    
+  useEffect(() => {
+      const fetchImages = async () => {
+          try {
+              const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
+              const data = await response.json();
+              
+              // fetch pokemon images from urls.
+              const pokemonDetails = await Promise.all(
+                  data.results.map(async (poke) => {
+                      const res = await fetch(poke.url);
+                      const details = await res.json();
+                      return {
+                          id: details.id,
+                          name: poke.name,
+                          image: details.sprites.front_default,
+                      };
+                  })
+              );
+              
+              setPokemon(pokemonDetails);
+          }catch (error) {
+              console.log("Error fetching data:", error)
+          }
+          };
+          fetchImages();
+      }, []);
 
   return (
     <>
